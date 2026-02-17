8e2c677d0800bc4a34c342c09d1e35b5e864502e
MichalGalas <michal.galas@catobe.pl>
2026-02-17 10:45:28 +0100

    Add solutions for 2015 Day 8 Parts 1 and 2
    Implemented Solver.cs for both parts of Day 8, handling string literal encoding and decoding logic. Added input and result files for Day 8, and updated the project structure to include new folders for Day 8 and other days.

---
+++ src/AoC_2015/AdventOfCode.Year2015/Day08/Part1/Solver.cs
@@ -0,0 +1,68 @@
+﻿using AdventOfCode.Commons;
+using AdventOfCodeGate.Interfaces;
+
+namespace AdventOfCode.Year2015.Day08.Part1;
+
+internal class Solver : BaseResolver, IAdventOfCode
+{
+	public override string Solve(string input)
+	{
+		var span = input.AsSpan().Trim();
+		var lineRanges = span.Split('\n');
+		var result = 0;
+		foreach ( var range in lineRanges )
+		{
+			var line = span[range];
+
+			result += ProcessLine(line);
+		}
+
+		return result.ToString();
+	}
+
+	private int ProcessLine(ReadOnlySpan<char> line)
+	{
+		var length = line.Length;
+		var result = -2;
+		bool specialChar = false;
+		int specialX = 0;
+		foreach (var c in line)
+		{
+			if (specialChar && specialX > 0)
+			{
+				specialX++;
+				if (specialX > 2)
+				{
+					specialChar = false;
+					specialX = 0;
+					result++;
+				}
+			}
+			else if (c == '\\')
+			{
+				if (!specialChar)
+					specialChar = true;
+				else
+				{
+					specialChar = false;
+					result++;
+				}
+			}
+			else if (specialChar && c == 'x')
+			{
+				specialX++;
+			}
+			else if (specialChar && c == '\"')
+			{
+				specialChar = false;
+				result++;
+			}
+			else
+			{ 
+				result++;
+			}
+		}
+
+		return length - result;
+	}
+}
---
+++ src/AoC_2015/AdventOfCode.Year2015/Day08/Part2/Solver.cs
@@ -0,0 +1,41 @@
+﻿using AdventOfCode.Commons;
+using AdventOfCodeGate.Interfaces;
+
+namespace AdventOfCode.Year2015.Day08.Part2;
+
+internal class Solver : BaseResolver, IAdventOfCode
+{
+	public override string Solve(string input)
+	{
+		var span = input.AsSpan().Trim();
+		var lineRanges = span.Split('\n');
+		var result = 0;
+		foreach ( var range in lineRanges )
+		{
+			var line = span[range];
+
+			result += ProcessLine(line);
+		}
+
+		return result.ToString();
+	}
+
+	private int ProcessLine(ReadOnlySpan<char> line)
+	{
+		var length = line.Length;
+		var result = 2;
+		foreach (var c in line)
+		{
+			if (c == '\\' || c == '\"')
+			{
+				result += 2;
+			}
+			else
+			{
+				result++;
+			}
+		}
+
+		return result - length;
+	}
+}
