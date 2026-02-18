934b119a4c1e24155bc6de448a74932371f5b4c9
MichalGalas <michal.galas@catobe.pl>
2026-02-18 08:10:09 +0100

    Solve Day09

---
+++ src/AoC_2015/AdventOfCode.Year2015/Day09/Common/CitiesPair.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace AdventOfCode.Year2015.Day09.Common;
+
+internal struct CitiesPair(string firstCity, string secondCity)
+{
+	public string FirstCity { get; set; } = firstCity;
+	public string SecondCity { get; set; } = secondCity;
+
+	public override bool Equals([NotNullWhen(true)] object? obj)
+	{
+		if (obj == null)
+			return false;
+
+		if (obj is CitiesPair cp)
+		{
+			return (this.FirstCity == cp.FirstCity && this.SecondCity == cp.SecondCity) ||
+						 (this.FirstCity == cp.SecondCity && this.SecondCity == cp.FirstCity);
+		}
+
+		return false;
+	}
+
+	public override int GetHashCode()
+	{
+		return 0;
+	}
+}
---
+++ src/AoC_2015/AdventOfCode.Year2015/Day09/Part2/Solver.cs
@@ -0,0 +1,106 @@
+﻿using AdventOfCode.Commons;
+using AdventOfCode.Year2015.Day09.Common;
+using AdventOfCodeGate.Interfaces;
+
+namespace AdventOfCode.Year2015.Day09.Part2;
+
+internal class Solver : BaseResolver, IAdventOfCode
+{
+	private Dictionary<CitiesPair, int> _distancesDictionary = new Dictionary<CitiesPair, int>();
+	private string[] _cities;
+	private List<int[]> _posibilities;
+	public override string Solve(string input)
+	{
+		_distancesDictionary.Clear();
+		_cities = [];
+		_posibilities = new List<int[]>();
+
+		var span = input.AsSpan().Trim();
+		var distancesRanges = span.Split('\n');
+		var cities = new List<string>();
+
+		foreach (var range in distancesRanges)
+			AddDistance(span[range], cities);
+
+		_cities = cities.ToArray();
+
+		CreatePosibilities(_cities.Length);
+
+		var maxDistance = GetMinimumDistance();
+
+		return maxDistance.ToString();
+	}
+
+	private int GetMinimumDistance()
+	{
+		var max = int.MinValue;
+
+		foreach (var item in _posibilities)
+		{
+			var distance = GetFullDistance(item);
+			if (distance > max)
+				max = distance;
+		}
+
+		return max;
+	}
+
+	private void AddDistance(ReadOnlySpan<char> line, List<string> cities)
+	{
+		var toIndex = line.IndexOf(" to ");
+		var equalIndex = line.IndexOf('=');
+
+		var firstCity = line[..toIndex].ToString();
+		var secondCity = line[(toIndex + 4)..(equalIndex - 1)].ToString();
+
+		if (!cities.Contains(secondCity))
+			cities.Add(secondCity);
+		if (!cities.Contains(firstCity))
+			cities.Add(firstCity);
+
+		var citiesPair = new CitiesPair(firstCity, secondCity);
+		var distance = int.Parse(line[(equalIndex + 2)..].ToString());
+
+		_distancesDictionary[citiesPair] = distance;
+	}
+
+	private void CreatePosibilities(int length)
+	{
+		var referenceList = new List<int>();
+		for (int i = 0; i < length; i++)
+			referenceList.Add(i);
+
+		var tempList = new List<int>();
+
+		Recurency(referenceList, tempList, _posibilities);
+	}
+
+	private void Recurency(List<int> referenceList, List<int> tempList, List<int[]> posibilities)
+	{
+		if (referenceList.Count == 0)
+		{
+			posibilities.Add(tempList.ToArray());
+			return;
+		}
+
+		foreach (int i in referenceList)
+		{
+			var newReference = referenceList.Where(x => x != i).ToList();
+			var newTemp = tempList.ToList();
+			newTemp.Add(i);
+			Recurency(newReference, newTemp, posibilities);
+		}
+	}
+
+	private int GetFullDistance(int[] item)
+	{
+		var span = _cities.AsSpan();
+		var distance = 0;
+		for (int i = 0; i < item.Length - 1; i++)
+		{
+			var pair = new CitiesPair(span[item[i]], span[item[i+1]]);
+			distance += _distancesDictionary[pair];
+		}
+		return distance;
+	}
+}
