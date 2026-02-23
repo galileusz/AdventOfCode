7802b3401692d8a7bd8b06a644459cfe4d354dd6
MichalGalas <michal.galas@catobe.pl>
2026-02-23 12:23:06 +0100

    Implement Day 11, only upper bound in recurency. lower bound must to be implemented

---
+++ src/AoC_2015/AdventOfCode.Year2015/Day11/Commons/PasswordContext.cs
@@ -0,0 +1,51 @@
+﻿namespace AdventOfCode.Year2015.Day11.Commons;
+
+internal class PasswordContext
+{
+	public bool IsFirstPairRuleRealized { get; private set; } = false;
+	public bool IsSecondPairRuleRealized { get; private set; } = false;
+	public bool IsStraightRuleRealized { get; private set; } = false;
+	public int FirstPairRuleLevel { get; private set; } = 0;
+	public int SecondPairRuleLevel { get; private set; } = 0;
+	public int StraightRuleLevel { get; private set; } = 0;
+
+	public void SetFirstPairRuleRealized(int level)
+	{
+				IsFirstPairRuleRealized = true;
+				FirstPairRuleLevel = level;
+	}
+
+	public void SetSecondPairRuleRealized(int level)
+	{
+				IsSecondPairRuleRealized = true;
+				SecondPairRuleLevel = level;
+	}
+
+	public void SetStraightRuleRealized(int level)
+	{
+				IsStraightRuleRealized = true;
+				StraightRuleLevel = level;
+	}
+
+	public void ResetRulesRealization(int level)
+	{
+		if (IsFirstPairRuleRealized && FirstPairRuleLevel >= level)
+		{
+			IsFirstPairRuleRealized = false;
+			FirstPairRuleLevel = 0;
+		}
+		if (IsSecondPairRuleRealized && SecondPairRuleLevel >= level)
+		{
+			IsSecondPairRuleRealized = false;
+			SecondPairRuleLevel = 0;
+		}
+		if (IsStraightRuleRealized && StraightRuleLevel >= level)
+		{
+			IsStraightRuleRealized = false;
+			StraightRuleLevel = 0;
+		}
+	}
+
+	public bool AreAllRulesRealized() =>
+		IsFirstPairRuleRealized && IsSecondPairRuleRealized && IsStraightRuleRealized;
+}
---
+++ src/AoC_2015/AdventOfCode.Year2015/Day11/Part1/Solver.cs
@@ -0,0 +1,116 @@
+﻿using AdventOfCode.Commons;
+using AdventOfCode.Year2015.Day10.Commons;
+using AdventOfCode.Year2015.Day11.Commons;
+using AdventOfCodeGate.Interfaces;
+
+namespace AdventOfCode.Year2015.Day11.Part1;
+
+internal class Solver : BaseResolver, IAdventOfCode
+{
+	private const int _maxLetter = 122;
+	private const int _minLetter = 97;
+	private bool _firstRun = true;
+
+	public override string Solve(string input)
+	{
+		var span = input.AsSpan().Trim();
+		var maxLevel = span.Length - 1;
+		var password = new int[span.Length];
+
+		var context = new PasswordContext();
+
+		ProcessRecurency(span, password, 0, context, maxLevel);
+
+		return span.Length.ToString();
+	}
+
+	private void ProcessRecurency(ReadOnlySpan<char> span, int[] password, int level, PasswordContext context, int maxLevel)
+	{
+		if (level > maxLevel)
+		{
+			Console.WriteLine($"{new String(password.Select(x => (char)x).ToArray())}");
+			_firstRun = false;
+			return;
+		}
+
+		if (context.AreAllRulesRealized())
+		{
+			return;
+		}
+
+		context.ResetRulesRealization(level);
+
+		var minLetter = _firstRun ? span[level] : _minLetter;
+
+		for (var i = minLetter; i <= _maxLetter; i++)
+		{
+			if (IsLetterToSkip(i))
+				continue;
+
+			password[level] = i;
+
+			if (!context.IsFirstPairRuleRealized && level > 0)
+				CheckFirstPair(password, context, level);
+
+			if (context.IsFirstPairRuleRealized && !context.IsSecondPairRuleRealized)
+				CheckSecondPair(password, context, level);
+
+			if (context.IsStraightRuleRealized && level > 1)
+				CheckStraightRule(password, context, level);
+
+			ProcessRecurency(span, password, level + 1, context, maxLevel);
+		}
+	}
+
+	private void CheckStraightRule(int[] password, PasswordContext context, int level)
+	{
+		var straightCount = 1;
+		var previous = password[0];
+
+		for (var i = 1; i <= level; i++)
+		{
+			if (password[i] - previous == 1)
+			{
+				straightCount++;
+				if (straightCount == 3)
+				{
+					context.SetStraightRuleRealized(i);
+					return;
+				}
+			}
+			else
+			{
+				straightCount = 1;
+			}
+
+			previous = i;
+		}
+	}
+
+	private void CheckFirstPair(int[] password, PasswordContext context, int level)
+	{
+		for (int i = 0; i < level; i++)
+		{
+			if (password[i] == password[i+1])
+			{
+				context.SetFirstPairRuleRealized(i+1);
+				return;
+			}
+		}
+	}
+
+	private void CheckSecondPair(int[] password, PasswordContext context, int level)
+	{
+		for (int i = context.FirstPairRuleLevel + 2; i < level; i++)
+		{
+			if (password[i] == password[i + 1])
+			{
+				context.SetSecondPairRuleRealized(i + 1);
+				return;
+			}
+		}
+	}
+
+	private bool IsLetterToSkip(int letter) =>
+		letter == 105 || letter == 108 || letter == 111;
+}
