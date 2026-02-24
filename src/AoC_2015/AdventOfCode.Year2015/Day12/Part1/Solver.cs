fc1bde5578948df0f58473bfe9c8e714fde403c2
MichalGalas <michal.galas@catobe.pl>
2026-02-24 14:49:26 +0100

    Add solutions for Day 12 of Advent of Code 2015
    Implemented solvers for Day 12 Part 1 and Part 2. The Part 1 solver calculates the sum of all numbers in a JSON-like input, ignoring numbers inside strings. The Part 2 solver skips objects containing the string "red" while summing numbers.
    
    Updated `AdventOfCode.Year2015.csproj` to remove Day 12 folders and add placeholders for Day 13. Added `Day12_DataInput.txt` with a complex JSON structure as input and `Day12_Part1_Result.txt` with the result of Part 1 for verification.

---
+++ src/AoC_2015/AdventOfCode.Year2015/Day12/Part1/Solver.cs
@@ -0,0 +1,51 @@
+﻿using AdventOfCode.Commons;
+using AdventOfCodeGate.Interfaces;
+using System.Text;
+
+namespace AdventOfCode.Year2015.Day12.Part1;
+
+internal class Solver : BaseResolver, IAdventOfCode
+{
+	public override string Solve(string input)
+	{
+		var sum = 0;
+		var span = input.AsSpan().Trim();
+		var stringStarted = false;
+		var numberStarted = false;
+		var numberBuilder = new StringBuilder();
+
+
+		foreach (var c in span)
+		{
+			if (c == '\"')
+				stringStarted = !stringStarted;
+
+			if (stringStarted)
+				continue;
+
+			if (numberStarted && char.IsAsciiDigit(c))
+			{
+				numberBuilder.Append(c);
+				continue;
+			}
+
+			if (numberStarted && !char.IsDigit(c))
+			{
+				if (int.TryParse(numberBuilder.ToString(), out var number))
+					sum += number;
+
+				numberBuilder.Clear();
+				numberStarted = false;
+				continue;
+			}
+
+			if (c == '-' || char.IsDigit(c))
+			{
+				numberStarted = true;
+				numberBuilder.Append(c);
+			}
+		}
+		
+		return sum.ToString();
+	}
+}
---
+++ src/AoC_2015/AdventOfCode.Year2015/Day12/Part2/Solver.cs
@@ -0,0 +1,99 @@
+﻿using AdventOfCode.Commons;
+using AdventOfCodeGate.Interfaces;
+using System.Text;
+
+namespace AdventOfCode.Year2015.Day12.Part2;
+
+internal class Solver : BaseResolver, IAdventOfCode
+{
+	public override string Solve(string input)
+	{
+		var sum = 0;
+		var span = input.AsSpan().Trim();
+		var valueStarted = false;
+		var stringStarted = false;
+		var numberStarted = false;
+		var numberBuilder = new StringBuilder();
+		var startCheckRed = false;
+		var previousIsR = false;
+		var previousIsE = false;
+		var previousIsD = false;
+		var isRedObject = false;
+
+		var tempSums = new int[30];
+
+
+		foreach (var c in span)
+		{
+			if (c == ':')
+				valueStarted = true;
+
+			if (c == '{' || c == '}' || c == '[' || c == ']' || c == ',')
+				valueStarted = false;
+
+			if (c == '\"')
+				stringStarted = !stringStarted;
+
+			if (stringStarted && !valueStarted)
+				continue;
+
+			if (stringStarted && valueStarted)
+			{
+				if (c == '\"')
+					startCheckRed = true;
+				else if (c == 'r' && startCheckRed)
+				{
+					startCheckRed = false;
+					previousIsR = true;
+				}
+				else if (c == 'e' && previousIsR)
+				{
+					previousIsR = false;
+					previousIsE = true;
+				}
+				else if (c == 'd' && previousIsE)
+				{
+					previousIsE = false;
+					previousIsD = true;
+				}
+				else if (c == '\"' && previousIsD)
+				{
+					previousIsD = false;
+					numberStarted = false;
+					isRedObject = true;
+				}
+				else
+				{
+					startCheckRed = false;
+					previousIsR = false;
+					previousIsE = false;
+					previousIsD = false;
+				}
+			}
+
+			if (numberStarted && char.IsAsciiDigit(c))
+			{
+				numberBuilder.Append(c);
+				continue;
+			}
+
+			if (numberStarted && !char.IsDigit(c))
+			{
+				if (int.TryParse(numberBuilder.ToString(), out var number))
+					sum += number;
+
+				numberBuilder.Clear();
+				numberStarted = false;
+				continue;
+			}
+
+			if (c == '-' || char.IsDigit(c))
+			{
+				numberStarted = true;
+				numberBuilder.Append(c);
+			}
+		}
+		
+		return sum.ToString();
+	}
+}
