77175149f08dd3445d9733f949af909dd4de9d83
MichalGalas <michal.galas@catobe.pl>
2026-03-05 13:12:32 +0100

    Add Day 16 solutions for Advent of Code 2015
    Implemented solutions for Day 16, Parts 1 and 2, of Advent of Code 2015.
    
    - Updated `AdventOfCode.Year2015.csproj` to reorganize project structure by removing `Day16\Part1` and `Day16\Part2` folders.
    - Added `Solver.cs` for Part 1 under `AdventOfCode.Year2015.Day16.Part1` with logic to identify the correct "Sue" based on exact attribute matches.
    - Added `Solver.cs` for Part 2 under `AdventOfCode.Year2015.Day16.Part2` with logic to handle modified attribute comparison rules.
    - Added `Day16_DataInput.txt` containing the input data for both parts.
    - Added result files: `Day16_Part1_Result.txt` (213) and `Day16_Part2_Result.txt` (323) to store the solutions' outputs.

---
+++ src/AoC_2015/AdventOfCode.Year2015/Day16/Part2/Solver.cs
@@ -0,0 +1,115 @@
+﻿using AdventOfCode.Commons;
+using AdventOfCodeGate.Interfaces;
+
+namespace AdventOfCode.Year2015.Day16.Part2;
+
+internal class Solver : BaseResolver, IAdventOfCode
+{
+	public override string Solve(string input)
+	{
+		var span = input.AsSpan().Trim();
+		var lines = span.Split('\n');
+
+		var resultIndex = 0;
+		foreach (var line in lines)
+		{
+			resultIndex++;
+			if (IsCorrectSue(span[line]))
+				break;
+		}
+
+		return resultIndex.ToString();
+	}
+
+	private bool IsCorrectSue(ReadOnlySpan<char> span)
+	{
+		if (!ChildrenOk(span))
+			return false;
+
+		if (!CatsOk(span))
+			return false;
+
+		if (!SamoyedsOk(span))
+			return false;
+
+		if (!PomeraniansOk(span))
+			return false;
+
+		if (!AkitasOk(span))
+			return false;
+
+		if (!VizslasOk(span))
+			return false;
+
+		if (!GoldfishOk(span))
+			return false;
+
+		if (!TreesOk(span))
+			return false;
+
+		if (!CarsOk(span))
+			return false;
+
+		if (!PerfumesOk(span))
+			return false;
+
+		return true;
+	}
+
+	private bool ChildrenOk(ReadOnlySpan<char> span) => ValueEqual(span, "children: ", '3');
+
+	private bool CatsOk(ReadOnlySpan<char> span) => ValueGreater(span, "cats: ", '7');
+
+	private bool SamoyedsOk(ReadOnlySpan<char> span) => ValueEqual(span, "samoyeds: ", '2');
+
+	private bool PomeraniansOk(ReadOnlySpan<char> span) => ValueLower(span, "pomeranians: ", '3');
+
+	private bool AkitasOk(ReadOnlySpan<char> span) => ValueEqual(span, "akitas: ", '0');
+
+	private bool VizslasOk(ReadOnlySpan<char> span) => ValueEqual(span, "vizslas: ", '0');
+
+	private bool GoldfishOk(ReadOnlySpan<char> span) => ValueLower(span, "goldfish: ", '5');
+
+	private bool TreesOk(ReadOnlySpan<char> span) => ValueGreater(span, "trees: ", '3');
+
+	private bool CarsOk(ReadOnlySpan<char> span) => ValueEqual(span, "cars: ", '2');
+
+	private bool PerfumesOk(ReadOnlySpan<char> span) => ValueEqual(span, "perfumes: ", '1');
+
+	private bool ValueEqual(ReadOnlySpan<char> span, ReadOnlySpan<char> name, char expectedValue)
+	{
+		var index = span.IndexOf(name);
+		if (index == -1)
+			return true;
+
+		return span[index + name.Length] == expectedValue;
+	}
+
+	private bool ValueGreater(ReadOnlySpan<char> span, ReadOnlySpan<char> name, char expectedValue)
+	{
+		var index = span.IndexOf(name);
+		if (index == -1)
+			return true;
+
+		var valueIndex = index + name.Length;
+		if (span[valueIndex] > expectedValue)
+			return true;
+
+		if (span.Length - 1 == valueIndex)
+			return false;
+
+		if (span[valueIndex + 1] == ',')
+			return false;
+
+		return true;
+	}
+
+	private bool ValueLower(ReadOnlySpan<char> span, ReadOnlySpan<char> name, char expectedValue)
+	{
+		var index = span.IndexOf(name);
+		if (index == -1)
+			return true;
+
+		return span[index + name.Length] < expectedValue;
+	}
+}
