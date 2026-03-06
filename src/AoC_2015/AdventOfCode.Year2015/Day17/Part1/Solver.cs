62c625834c528df24b9d66e5c652659c89cebbc9
MichalGalas <michal.galas@catobe.pl>
2026-03-06 13:30:29 +0100

    Add Day 17 solutions and update project structure
    Implemented solutions for Day 17 Part 1 and Part 2:
    - Added `Solver` class for Part 1 to calculate combinations of containers summing to 150 using recursive logic and memory-efficient array pooling.
    - Added `Solver` class for Part 2 to extend Part 1 logic, tracking the minimum number of containers and their counts.
    
    Updated `AdventOfCode.Year2015.csproj`:
    - Replaced `Day17\Part1` and `Day17\Part2` folders with `Day18\Part1` and `Day18\Part2`.
    
    Added input and result files:
    - `Day17_DataInput.txt` with container sizes.
    - `Day17_Part1_Result.txt` with result `654`.
    - `Day17_Part2_Result.txt` with result `57`.

---
+++ src/AoC_2015/AdventOfCode.Year2015/Day17/Part1/Solver.cs
@@ -0,0 +1,94 @@
+﻿using AdventOfCode.Commons;
+using AdventOfCodeGate.Interfaces;
+using System.Buffers;
+
+namespace AdventOfCode.Year2015.Day17.Part1;
+
+internal class Solver : BaseResolver, IAdventOfCode
+{
+	private ArrayPool<int> _pool = null!;
+	private int _results;
+	public override string Solve(string input)
+	{
+		_results = 0;
+		var span = input.AsSpan().Trim();
+		_pool = ArrayPool<int>.Shared;
+		var array = _pool.Rent(30);
+		var maxIndex = 0;
+
+		GetData(span, array, ref maxIndex);
+
+		CheckPosibilities(array, maxIndex);
+
+		return _results.ToString();
+	}
+
+	private void GetData(ReadOnlySpan<char> span, int[] array, ref int maxIndex)
+	{
+		var currentNumber = 0;
+		maxIndex = 0;
+		foreach (var c in span)
+		{
+			if (c == '\n')
+			{
+				array[maxIndex++] = currentNumber;
+				currentNumber = 0;
+				continue;
+			}
+			currentNumber = currentNumber * 10 + (c - '0');
+		}
+		if (currentNumber != 0)
+			array[maxIndex] = currentNumber;
+	}
+
+	private void CheckPosibilities(int[] numbers, int maxIndex)
+	{
+		var referenceArray = _pool.Rent(maxIndex + 1);
+		for (var i = 1; i <= maxIndex + 1; i++)
+			referenceArray[i - 1] = i;
+
+		Recurency(numbers, referenceArray, maxIndex, 0);
+	}
+
+	private void Recurency(int[] numbers, int[] referenceArray, int maxIndex, int minIndex)
+	{
+		if (CheckIsFullfilled(numbers, referenceArray, maxIndex))
+		{
+			_pool.Return(referenceArray);
+			return;
+		}
+
+		for (var i = minIndex; i <= maxIndex; i++)
+		{
+			var newReference = _pool.Rent(referenceArray.Length);
+			Array.Copy(referenceArray, newReference, referenceArray.Length);
+			newReference[i] = 0;
+
+			Recurency(numbers, newReference, maxIndex, i + 1);
+		}
+	}
+
+	private bool CheckIsFullfilled(int[] numbers, int[] referenceArray, int maxIndex)
+	{
+		var sum = 0;
+		for (var i = 0; i <= maxIndex; i++)
+		{
+			if (referenceArray[i] == 0)
+			{
+				sum += numbers[i];
+			}
+			if (sum > 150)
+			{
+				return true;
+			}
+		}
+		if (sum == 150)
+		{
+			_results++;
+
+			return true;
+		}
+
+		return false;
+	}
+}
---
+++ src/AoC_2015/AdventOfCode.Year2015/Day17/Part2/Solver.cs
@@ -0,0 +1,97 @@
+﻿using AdventOfCode.Commons;
+using AdventOfCodeGate.Interfaces;
+using System.Buffers;
+
+namespace AdventOfCode.Year2015.Day17.Part2;
+
+internal class Solver : BaseResolver, IAdventOfCode
+{
+	private ArrayPool<int> _pool = null!;
+	private List<int> _results;
+	public override string Solve(string input)
+	{
+		_results = new List<int>();
+		var span = input.AsSpan().Trim();
+		_pool = ArrayPool<int>.Shared;
+		var array = _pool.Rent(30);
+		var maxIndex = 0;
+
+		GetData(span, array, ref maxIndex);
+
+		CheckPosibilities(array, maxIndex);
+
+		var min = _results.Min();
+		return _results.Count(x => x == min).ToString();
+	}
+
+	private void GetData(ReadOnlySpan<char> span, int[] array, ref int maxIndex)
+	{
+		var currentNumber = 0;
+		maxIndex = 0;
+		foreach (var c in span)
+		{
+			if (c == '\n')
+			{
+				array[maxIndex++] = currentNumber;
+				currentNumber = 0;
+				continue;
+			}
+			currentNumber = currentNumber * 10 + (c - '0');
+		}
+		if (currentNumber != 0)
+			array[maxIndex] = currentNumber;
+	}
+
+	private void CheckPosibilities(int[] numbers, int maxIndex)
+	{
+		var referenceArray = _pool.Rent(maxIndex + 1);
+		for (var i = 1; i <= maxIndex + 1; i++)
+			referenceArray[i - 1] = i;
+
+		Recurency(numbers, referenceArray, maxIndex, 0);
+	}
+
+	private void Recurency(int[] numbers, int[] referenceArray, int maxIndex, int minIndex)
+	{
+		if (CheckIsFullfilled(numbers, referenceArray, maxIndex))
+		{
+			_pool.Return(referenceArray);
+			return;
+		}
+
+		for (var i = minIndex; i <= maxIndex; i++)
+		{
+			var newReference = _pool.Rent(referenceArray.Length);
+			Array.Copy(referenceArray, newReference, referenceArray.Length);
+			newReference[i] = 0;
+
+			Recurency(numbers, newReference, maxIndex, i + 1);
+		}
+	}
+
+	private bool CheckIsFullfilled(int[] numbers, int[] referenceArray, int maxIndex)
+	{
+		var sum = 0;
+		var count = 0;
+		for (var i = 0; i <= maxIndex; i++)
+		{
+			if (referenceArray[i] == 0)
+			{
+				sum += numbers[i];
+				count++;
+			}
+			if (sum > 150)
+			{
+				return true;
+			}
+		}
+		if (sum == 150)
+		{
+			_results.Add(count);
+
+			return true;
+		}
+
+		return false;
+	}
+}
