a613612cbeb2ec3231ebfd835b4a1951b2f0b778
MichalGalas <michal.galas@catobe.pl>
2026-03-06 15:10:20 +0100

    Add Day 18 solutions and results for Advent of Code 2015
    Updated project structure to include Day 19 folders and added
    Day 18 solutions for Part 1 and Part 2. Implemented Conway's
    Game of Life simulation for a 100x100 grid over 100 iterations.
    
    - Added `Solver` classes for `Day18.Part1` and `Day18.Part2`.
      - Part 1: Standard simulation rules.
      - Part 2: Corner lights remain "on" throughout.
    - Added `Day18_DataInput.txt` for the initial grid state.
    - Added result files:
      - `Day18_Part1_Result.txt` with result `768`.
      - `Day18_Part2_Result.txt` with result `781`.

---
+++ src/AoC_2015/AdventOfCode.Year2015/Day18/Part1/Solver.cs
@@ -0,0 +1,84 @@
+﻿using AdventOfCode.Commons;
+using AdventOfCodeGate.Interfaces;
+
+namespace AdventOfCode.Year2015.Day18.Part1;
+
+internal class Solver : BaseResolver, IAdventOfCode
+{
+	public override string Solve(string input)
+	{
+		var array1 = new bool[10000];
+		var array2 = new bool[10000];
+
+		FillArray(array1, input.AsSpan());
+
+		bool[] source;
+		bool[] target = array2;
+		for (int i = 0; i < 100; i++)
+		{
+			source = i % 2 == 0 ? array1 : array2;
+			target = i % 2 == 0 ? array2 : array1;
+
+			RunLife(source, target);
+		}
+
+		return target.Count(x => x).ToString();
+	}
+
+	private void FillArray(bool[] array1, ReadOnlySpan<char> span)
+	{
+		var index = 0;
+		foreach (var c in span)
+		{
+			if (c == '\n')
+				continue;
+
+			array1[index] = c == '#';
+			index++;
+		}
+	}
+
+	private void RunLife(bool[] source, bool[] target)
+	{
+		for (int x = 0; x < 100; x++)
+		{
+			for (int y = 0; y < 100; y++)
+			{
+				var neighbors = GetNeighbors(source, x, y);
+
+				ChangeTarget(neighbors, source, target, x, y);
+			}
+		}
+	}
+
+	private int GetNeighbors(bool[] source, int x, int y)
+	{
+		var sum = 0;
+		for (int i = x - 1; i <= x + 1; i++)
+		{
+			for (int j = y - 1; j <= y + 1; j++)
+			{
+				if (i < 0 || j < 0 || i > 99 || j > 99 || (i == x && j == y))
+					continue;
+
+				if (source[i * 100 + j])
+					sum++;
+				if (sum > 3)
+					return 4;
+			}
+		}
+		return sum;
+	}
+
+	private void ChangeTarget(int neighbors, bool[] source, bool[] target, int x, int y)
+	{
+		if (source[x * 100 + y])
+		{
+			target[x * 100 + y] = neighbors == 2 || neighbors == 3;
+		}
+		else
+		{
+			target[x * 100 + y] = neighbors == 3;
+		}
+	}
+}
